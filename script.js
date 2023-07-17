2a24cd7f950190e1b88d1dc88ba77d96223dee92
Candace Turner <131117210+CandyRain89@users.noreply.github.com>
2023-07-17 10:01:59 -0400

    i added the pages for the nav

--- script.js
+++ script.js
@@ -52,18 +52,24 @@ const navLinks = document.querySelectorAll('.nav-list li a');
 
 navLinks.forEach((link) => {
   link.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent the default link behavior
+    event.preventDefault(); 
 
-    // Get the target section or page based on the link's href attribute
     const target = document.querySelector(link.getAttribute('href'));
 
-    // Scroll to the target section or navigate to the target page
-    // You can use different methods based on your requirements
-
-    // Example 1: Scroll to the target section smoothly
     target.scrollIntoView({ behavior: 'smooth' });
+    
+  });
+});
 
-    // Example 2: Navigate to the target page using window.location.href
-    // window.location.href = link.getAttribute('href');
+const navLinks = document.querySelectorAll('.nav-list li a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); 
+
+   
+    const target = document.querySelector(link.getAttribute('href'));
+
+    target.scrollIntoView({ behavior: 'smooth' });
   });
 });
